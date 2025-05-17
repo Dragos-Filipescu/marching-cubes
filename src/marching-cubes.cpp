53a36ba73ab9a5911b80febee5fb9fb63f3aa765
Dragos-Filipescu <dragosfilipescu255@gmail.com>
2025-05-17 18:14:46 +0300

    Add project files.

---
+++ src/marching-cubes.cpp
@@ -0,0 +1,12 @@
+﻿// marching-cubes.cpp : Defines the entry point for the application.
+//
+
+#include "marching-cubes.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
